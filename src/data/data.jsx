de2bfea92da308a3ea49e8cd9e4585aed3aa6642
MaksSurzhko <117912210+MaksSurzhko@users.noreply.github.com>
2023-05-25 20:13:21 +0300

    hw

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,27 +1,28 @@
+import PropTypes from 'prop-types';
 
 const Datas = ({ id, label, percentage }) => {
     return (
         <section class="statistics">
-            <h2 class="title">{data.id}</h2>
+  <h2 class="title">Upload stats</h2>
 
-            <ul class="stat-list">
-                <li class="item">
-                    <span class="label">{data.label}</span>
-                    <span class="percentage">{data.percentage}</span>
-                </li>
-                <li class="item">
-                    <span class="label">{data.label}</span>
-                    <span class="percentage">{data.percentage}</span>
-                </li>
-                <li class="item">
-                    <span class="label">{data.label}</span>
-                    <span class="percentage">{data.percentage}</span>
-                </li>
-                <li class="item">
-                    <span class="label">{data.label}</span>
-                    <span class="percentage">{data.percentage}</span>
-                </li>
-            </ul>
-        </section>
+  <ul class="stat-list">
+    <li class="item">
+      <span class="label">.docx</span>
+      <span class="percentage">4%</span>
+    </li>
+    <li class="item">
+      <span class="label">.mp3</span>
+      <span class="percentage">14%</span>
+    </li>
+    <li class="item">
+      <span class="label">.pdf</span>
+      <span class="percentage">41%</span>
+    </li>
+    <li class="item">
+      <span class="label">.mp4</span>
+      <span class="percentage">12%</span>
+    </li>
+  </ul>
+</section>
     );
 }; export default Datas
