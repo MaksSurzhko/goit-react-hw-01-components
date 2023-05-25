dba58711c2b2da616182a12431129795f48c57cf
MaksSurzhko <117912210+MaksSurzhko@users.noreply.github.com>
2023-05-25 18:45:08 +0300

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-export const App = () => {
+/*export const App = () => {
   return (
     <div
       style={{
@@ -13,4 +13,31 @@ export const App = () => {
       React homework template
     </div>
   );
-};
+};*/
+
+
+import Profile from "../users/Profile"
+import user from "../users/user.json"
+import data from "../data/data"
+
+
+export default function App() {
+  return(
+    <div>
+      <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+      followers={user.followers}
+      views={user.views} 
+      likes={user.likes}
+      />
+    </div>
+  )
+}
+
+
+
+
---
+++ src/data/data.jsx
@@ -0,0 +1,24 @@
+
+
+<section class="statistics">
+    <h2 class="title">{data.id}</h2>
+
+  <ul class="stat-list">
+    <li class="item">
+            <span class="label">{label}</span>
+      <span class="percentage">{percentage}</span>
+    </li>
+    <li class="item">
+      <span class="label">{label}</span>
+      <span class="percentage">{percentage}</span>
+    </li>
+    <li class="item">
+      <span class="label">{label}</span>
+      <span class="percentage">{percentage}</span>
+    </li>
+    <li class="item">
+      <span class="label">.mp4</span>
+      <span class="percentage">12%</span>
+    </li>
+  </ul>
+</section>
